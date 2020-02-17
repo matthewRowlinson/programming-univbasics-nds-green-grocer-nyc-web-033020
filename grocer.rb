ae52b1bcfd92248f7541bf69529ad516c9e2f2f1
matthew rowlinson <matthewrowlinson1@gmail.com>
2020-02-17 17:22:24 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -78,17 +78,17 @@ def checkout(cart, coupons)
   #
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
-  consolidated_cart = consolidate_cart(cart)
-  couponed_cart = apply_coupons(consolidated_cart, coupons)
-  final_cart = apply_clearance(couponed_cart)
-  total = 0 
-  i = 0 
-  while i < final_cart.length do 
-    total += final_cart[i][:price] * final_cart[i][:count]
-    i += 1 
-  end
-  if total > 100 
-    total -= (total * 0.10)
-  end
-  total 
+  # consolidated_cart = consolidate_cart(cart)
+  # couponed_cart = apply_coupons(consolidated_cart, coupons)
+  # final_cart = apply_clearance(couponed_cart)
+  # total = 0 
+  # i = 0 
+  # while i < final_cart.length do 
+  #   total += final_cart[i][:price] * final_cart[i][:count]
+  #   i += 1 
+  # end
+  # if total > 100 
+  #   total -= (total * 0.10)
+  # end
+  # total 
 end
