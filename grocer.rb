a7da5d835e379b88a2c9c93ffffe875f1d80da44
matthew rowlinson <matthewrowlinson1@gmail.com>
2020-02-17 17:03:35 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -31,8 +31,8 @@ def consolidate_cart(cart)
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
   new_cart = []
   i = 0 
-  while i < cart.length  
-    new_cart_item = find_item_by_name_in_collection(cart[i][:item], cart)
+  while i < cart.length do
+    new_cart_item = find_item_by_name_in_collection(cart[i][:item], new_cart)
     if new_cart_item
       new_cart_item[:count] += 1 
     else 
@@ -53,6 +53,29 @@ def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+  i = 0 
+  while i < coupons.length do 
+    cart_item = find_item_by_name_in_collection(coupons[i][:item], cart)
+    couponed_item_name = "#{coupons[i][:item]} W/COUPON"
+    cart_item_with_coupon = find_item_by_name_in_collection(couponed_item_name, cart)
+    if cart_item && cart_item[:count] >= coupons[i][:num]
+      if cart_item_with_coupon
+        cart_item_with_coupon[:count] += coupons[i][:num]
+        cart_item[:count] -= coupons[i][:num]
+      else 
+        cart_item_with_coupon = {
+          :item => couponed_item_name,
+          :price => coupons[i][:cost] / coupons[i][:num], 
+          :count => coupons[i][:num],
+          :clearance => cart_item[:clearance]
+        }
+        cart << cart_item_with_coupon
+        cart_item[:count] -= coupons[i][:num]
+      end
+    end
+    i += 1 
+  end 
+  cart
 end
 
 def apply_clearance(cart)
